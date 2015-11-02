1c121c0f5c5744fd4b8c796a2e43858243d8cf7a
cfkuyucu <cfkuyucu@gmail.com>
2015-11-02 23:50:17 +0200

    * Database Connections * Initialize database * added intialize link to home page * added counter page * added counter page link to home page * Initialize sample LEAGUES Table datas

--- server.py
+++ server.py
@@ -1,12 +1,23 @@
 import datetime
 import os
+import json
+import re
+import psycopg2 as dbapi2
 
-from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask.helpers import url_for
+from config import app
 
-
-app = Flask(__name__)
-
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 @app.route('/')
 def home_page():
@@ -37,6 +48,44 @@ def referees_page():
 def arenas_page():
     return render_template('arenas.html')
 
+@app.route('/initdb')
+def initialize_database():
+    connection = dbapi2.connect(app.config['dsn'])
+    cursor =connection.cursor()
+
+    query = """DROP TABLE IF EXISTS COUNTER"""
+    cursor.execute(query)
+    query = """CREATE TABLE COUNTER (N INTEGER)"""
+    cursor.execute(query)
+    query = """INSERT INTO COUNTER(N) VALUES(0)"""
+    cursor.execute(query)
+
+    query = """DROP TABLE IF EXISTS LEAGUES"""
+    cursor.execute(query)
+    query = """CREATE TABLE LEAGUES (ID SERIAL PRIMARY KEY, League_Name VARCHAR NOT NULL, League_Logo VARCHAR, League_Start_Date INTEGER, Country_ID INTEGER NOT NULL)"""
+    cursor.execute(query)
+    query = """INSERT INTO LEAGUES (League_Name,League_Logo,Country_ID) VALUES ('Super Lig','http://dwmdwmdk.com/img/logo1.jpg',1)"""
+    cursor.execute(query)
+    query = """INSERT INTO LEAGUES (League_Name,League_Logo,Country_ID) VALUES ('Premier Lig','http://dwmdwmdk.com/img/logo1.jpg',2)"""
+    cursor.execute(query)
+
+    connection.commit()
+    return redirect(url_for('home_page'))
+
+@app.route('/count')
+def counter_page():
+    connection = dbapi2.connect(app.config['dsn'])
+    cursor = connection.cursor()
+
+    query = "UPDATE COUNTER SET N = N + 1"
+    cursor.execute(query)
+    connection.commit()
+
+    query = "SELECT N FROM COUNTER"
+    cursor.execute(query)
+    count = cursor.fetchone()[0]
+
+    return "This page was accesed %d times." % count
 
 
 if __name__ == '__main__':
@@ -45,4 +94,11 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='postgres' password='12345'
+                               host='localhost' port=5432 dbname='itucsdb'"""
     app.run(host='0.0.0.0', port=port, debug=debug)
