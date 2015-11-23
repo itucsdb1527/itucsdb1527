fd360d3f671e003e44ce8abde9411b45030cd5a0
mericturan <mericturan94@gmail.com>
2015-11-23 20:42:03 +0200

    - added update option for teams page

--- teams.py
+++ teams.py
@@ -6,20 +6,99 @@ from flask import request
 from flask.helpers import url_for
 from config import app
 
+@app.route('/teams/initdb')
+def initialize_database_teams():
+    connection = dbapi2.connect(app.config['dsn'])
+    cursor =connection.cursor()
+
+    #Teams Table
+    query = """DROP TABLE IF EXISTS TEAMS CASCADE"""
+    cursor.execute(query)
+
+    query = """CREATE TABLE TEAMS(
+    ID SERIAL PRIMARY KEY,
+    Team_Name VARCHAR NOT NULL
+    )"""
+    cursor.execute(query)
+
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Galatasaray')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Besiktas')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Fenerbahce')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Ziraat Bankasi')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Halkbank')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Istanbul B.S.B.')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Sahinbey Bld.')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Maliye Milli Piyango')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Tokat Bld. Plevne')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Inegol Bld.')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Arkas Spor')"""
+    cursor.execute(query)
+    query = """INSERT INTO TEAMS (Team_Name) VALUES ('Bornova Anadolu Lisesi')"""
+    cursor.execute(query)
+
+
+    #Season Team Table
+    query = """DROP TABLE IF EXISTS SEASON_TEAM CASCADE"""
+    cursor.execute(query)
+
+    query = """CREATE TABLE SEASON_TEAM(
+    Season_ID INTEGER,
+    Team_ID INTEGER,
+    PRIMARY KEY(Season_ID, Team_ID),
+    FOREIGN KEY (Season_ID) REFERENCES SEASONS(ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Team_ID) REFERENCES TEAMS(ID) ON DELETE CASCADE ON UPDATE CASCADE
+    )"""
+    cursor.execute(query)
+
+    query = """INSERT INTO SEASON_TEAM (Season_ID, Team_ID) VALUES (1, 1)"""
+    cursor.execute(query)
+    query = """INSERT INTO SEASON_TEAM (Season_ID, Team_ID) VALUES (1, 2)"""
+    cursor.execute(query)
+
+
+    #Admin Table
+    query = """DROP TABLE IF EXISTS SITE CASCADE"""
+    cursor.execute(query)
+
+    query = """CREATE TABLE SITE(
+    Admin_Name VARCHAR NOT NULL,
+    Admin_Password VARCHAR NOT NULL,
+    Site_Name VARCHAR NOT NULL,
+    Slogan VARCHAR NOT NULL
+    )"""
+    cursor.execute(query)
+
+    query = """INSERT INTO SITE (Admin_Name, Admin_Password, Site_Name, Slogan) VALUES ('Meric','lolololo','itucsdb1527','Cimbombom')"""
+    cursor.execute(query)
+
+
+    connection.commit()
+    return redirect(url_for('teams_page'))
+
+
 @app.route('/teams', methods=['GET', 'POST'])
 def teams_page():
     connection = dbapi2.connect(app.config['dsn'])
     cursor = connection.cursor()
 
     if request.method == 'GET':
-        query = "SELECT T.ID, T.Team_Name, L.League_Name, T.League_ID FROM TEAMS T LEFT JOIN LEAGUES L ON (T.League_ID = L.ID)"
+        query = "SELECT ID, Team_Name FROM TEAMS ORDER BY ID"
         cursor.execute(query)
         return render_template('teams.html', teams = cursor)
     else:
         name = request.form['name']
-        league_id = request.form['league_id']        
-        query = """INSERT INTO TEAMS (Team_Name, League_ID)
-        VALUES ('"""+name+"', '"+league_id+"')"
+        query = """INSERT INTO TEAMS (Team_Name)
+        VALUES ('"""+name+"')"
         cursor.execute(query)
         connection.commit()
         return redirect(url_for('teams_page'))
@@ -34,46 +113,25 @@ def teams_page_delete(DELETEID):
         connection.commit()
         return redirect(url_for('teams_page'))
 
-@app.route('/teams/initdb')
-def initialize_database_teams():
+
+@app.route('/teams/UPDATE/<int:UPDATEID>/', methods=['GET', 'POST'])
+def teams_page_update(UPDATEID):
     connection = dbapi2.connect(app.config['dsn'])
-    cursor =connection.cursor()
-        
-    query = """DROP TABLE IF EXISTS TEAMS CASCADE"""
-    cursor.execute(query)
-    
-    query = """CREATE TABLE TEAMS(
-    ID SERIAL PRIMARY KEY, 
-    Team_Name VARCHAR NOT NULL, 
-    League_ID INTEGER,
-    FOREIGN KEY (League_ID) REFERENCES LEAGUES(ID) ON DELETE CASCADE
-    )"""
-    cursor.execute(query)
-    
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Galatasaray', 1)"""
-    cursor.execute(query)
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Besiktas', 1)"""
-    cursor.execute(query)
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Fenerbahce', 1)"""
-    cursor.execute(query)
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Ziraat Bankasi', 1)"""
-    cursor.execute(query)    
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Halkbank', 1)"""
-    cursor.execute(query)
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Istanbul B.S.B.', 1)"""
-    cursor.execute(query)
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Sahinbey Bld.', 1)"""
-    cursor.execute(query)
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Maliye Milli Piyango', 1)"""
-    cursor.execute(query)
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Tokat Bld. Plevne', 1)"""
-    cursor.execute(query)
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Inegol Bld.', 1)"""
-    cursor.execute(query)        
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Arkas Spor', 1)"""
-    cursor.execute(query)   
-    query = """INSERT INTO TEAMS (Team_Name, League_ID) VALUES ('Bornova Anadolu Lisesi', 1)"""
-    cursor.execute(query)   
-        
+    cursor = connection.cursor()
+
+    cursor.execute("""SELECT ID, Team_Name FROM TEAMS WHERE ID = %s""", (int(UPDATEID),))
     connection.commit()
-    return redirect(url_for('teams_page'))    
+    return render_template('teams_edit.html', teams = cursor)
+
+@app.route('/teams/UPDATE/<int:UPDATEID>/APPLY', methods=['GET', 'POST'])
+def teams_page_apply(UPDATEID):
+    connection = dbapi2.connect(app.config['dsn'])
+    cursor = connection.cursor()
+
+    new_name = request.form['name']
+    query = """UPDATE TEAMS SET Team_Name = '%s' WHERE ID = %d""" % (new_name, int(UPDATEID))
+    cursor.execute(query)
+    connection.commit()
+    return redirect(url_for('teams_page'))
+
+
